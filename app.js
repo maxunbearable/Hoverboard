86246049ada03c4ad3c1f0e43bf3a22e65016bff
max_unbearable <hypnofroghs@gmail.com>
2021-11-24 19:10:05 +0200

    created

---
+++ app.js
@@ -0,0 +1,44 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 1044;
+const header = document.querySelector('.page-header')
+const footer = document.querySelector('.footer-text')
+
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+const square = document.createElement('div')
+square.classList.add('square')
+
+square.addEventListener('mouseover', () => {
+    setColor(square)
+})
+square.addEventListener('mouseleave', () => {
+    removeColor(square)
+})
+board.append(square)
+}
+
+function setColor(element) {
+    let RandomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' +    colorValue() + ')';
+    const color = RandomColor;
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    header.style.color = color;
+    header.style.textShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    footer.style.color = color;
+    footer.style.textShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '';
+    element.style.boxShadow = ``;
+    header.style.textShadow = ``;
+    header.style.color = '';
+    footer.style.textShadow = ``;
+    footer.style.color = '';
+
+}
+
+function colorValue() {
+    return Math.floor(Math.random() * 256);
+  }
